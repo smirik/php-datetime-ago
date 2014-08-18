51fd42cf8b108619dd46166a942a2a2e64348d00
Riccardo Franconi <riccardo@MacBook-Pro-di-Riccardo-2.local>
2014-08-18 16:10:51 +0200

    Adds italian text translator

--- spec/Smirik/PHPDateTimeAgo/DateTimeAgoSpec.php
+++ spec/Smirik/PHPDateTimeAgo/DateTimeAgoSpec.php
@@ -7,12 +7,12 @@ use Prophecy\Argument;
 
 class DateTimeAgoSpec extends ObjectBehavior
 {
-    
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Smirik\PHPDateTimeAgo\DateTimeAgo');
     }
-    
+
     function it_checks_daily()
     {
         $now = new \DateTime();
@@ -37,8 +37,8 @@ class DateTimeAgoSpec extends ObjectBehavior
 
         $diff = $now->diff(new \DateTime('-2 hours'));
         $this->hourly($diff)->shouldBe(false);
-    }    
-    
+    }
+
     function it_checks_now()
     {
         $this->get(new \DateTime())->shouldReturn("now");
@@ -46,14 +46,14 @@ class DateTimeAgoSpec extends ObjectBehavior
         $this->get(new \DateTime('-25 seconds'))->shouldReturn("now");
         $this->get(new \DateTime('-59 seconds'))->shouldReturn("now");
     }
-    
+
     function it_checks_minutes()
     {
         $this->get(new \DateTime('-3 minutes'))->shouldBe('3 minutes ago');
         $this->get(new \DateTime('-25 minutes'))->shouldBe('25 minutes ago');
         $this->get(new \DateTime('-61 minutes'))->shouldBe('1 hour ago');
     }
-    
+
     function it_checks_hours()
     {
         $this->get(new \DateTime('-90 minutes'))->shouldBe('1 hour ago');
@@ -61,7 +61,7 @@ class DateTimeAgoSpec extends ObjectBehavior
         $this->get(new \DateTime('-120 minutes'))->shouldBe('2 hours ago');
         $this->get(new \DateTime('-2 hours'))->shouldBe('2 hours ago');
     }
-    
+
     function it_checks_days()
     {
         $this->get(new \DateTime('-24 hours'))->shouldBe('1 day ago');
@@ -70,14 +70,14 @@ class DateTimeAgoSpec extends ObjectBehavior
         $this->setMaxDaysCount(3);
         $this->get(new \DateTime('-5 days'))->shouldNotBe('5 days ago');
     }
-    
+
     function it_checks_format()
     {
         $this->get(new \DateTime('2001-01-01 23:59'), new \DateTime('2001-01-20 01:00'))->shouldBe('2001-01-01');
         $this->setFormat('Y-m-d H:i');
         $this->get(new \DateTime('2001-01-01 23:59'), new \DateTime('2001-01-20 01:00'))->shouldBe('2001-01-01 23:59');
     }
-    
+
     function it_checks_russian()
     {
         $this->setTextTranslator(new \Smirik\PHPDateTimeAgo\TextTranslator\RussianTextTranslator());
@@ -86,11 +86,32 @@ class DateTimeAgoSpec extends ObjectBehavior
         $this->get(new \DateTime('-25 minutes'))->shouldBe('25 минут назад');
         $this->get(new \DateTime('-59 minutes'))->shouldBe('59 минут назад');
         $this->get(new \DateTime('-61 minutes'))->shouldBe('1 час назад');
-        
+
         $this->get(new \DateTime('-24 hours'))->shouldBe('1 день назад');
         $this->get(new \DateTime('-2 days'))->shouldBe('2 дня назад');
         $this->get(new \DateTime('-5 days'))->shouldBe('5 дней назад');
-        
+
+    }
+
+    function it_checks_italian()
+    {
+        $this->setTextTranslator(new \Smirik\PHPDateTimeAgo\TextTranslator\ItalianTextTranslator());
+
+        $this->get(new \DateTime())->shouldBe('ora');
+        $this->get(new \DateTime('-5 seconds'))->shouldReturn('ora');
+        $this->get(new \DateTime('-25 seconds'))->shouldReturn('ora');
+        $this->get(new \DateTime('-59 seconds'))->shouldReturn('ora');
+
+        $this->get(new \DateTime('-1 minutes'))->shouldBe('1 minuto fa');
+        $this->get(new \DateTime('-3 minutes'))->shouldBe('3 minuti fa');
+        $this->get(new \DateTime('-25 minutes'))->shouldBe('25 minuti fa');
+        $this->get(new \DateTime('-59 minutes'))->shouldBe('59 minuti fa');
+        $this->get(new \DateTime('-61 minutes'))->shouldBe('1 ora fa');
+
+        $this->get(new \DateTime('-24 hours'))->shouldBe('1 giorno fa');
+        $this->get(new \DateTime('-2 days'))->shouldBe('2 giorni fa');
+        $this->get(new \DateTime('-5 days'))->shouldBe('5 giorni fa');
+
     }
 
 }
