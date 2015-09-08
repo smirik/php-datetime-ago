dc93474c22d64613b736fe92854715c555bc680f
Andrew Coulton <andrew@ingenerator.com>
2015-09-08 16:33:21 +0000

    Add optional support for weeks/months/years in English
    For backwards compatibility the behaviour is only available when the
    translator is configured with `->enableWeeksMonthsYears(TRUE)`.

--- src/Smirik/PHPDateTimeAgo/TextTranslator/EnglishTextTranslator.php
+++ src/Smirik/PHPDateTimeAgo/TextTranslator/EnglishTextTranslator.php
@@ -8,7 +8,22 @@ class EnglishTextTranslator extends AbstractTextTranslator
     protected $minute_words = array('minute ago', 'minutes ago');
     protected $hour_words   = array('hour ago', 'hours ago');
     protected $day_words    = array('day ago', 'days ago');
-    
+    protected $week_words   = array('week ago', 'weeks ago');
+    protected $month_words  = array('month ago', 'months ago');
+    protected $year_words   = array('year ago', 'years ago');
+
+    protected $weeks_months_years = FALSE;
+
+    /**
+     * Toggle whether to render weeks/months/years or fall back to date format for longer periods.
+     *
+     * @param bool $enable
+     */
+    public function enableWeeksMonthsYears($enable)
+    {
+        $this->weeks_months_years = $enable;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -24,5 +39,29 @@ class EnglishTextTranslator extends AbstractTextTranslator
     {
         return ($number == 1) ? 0 : 1;
     }
-    
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsWeeks()
+    {
+        return $this->weeks_months_years;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsMonths()
+    {
+        return $this->weeks_months_years;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsYears()
+    {
+        return $this->weeks_months_years;
+    }
+
 }
